2280ea35c69317485ff735d69800ee1646d8e193
Данияр <daniyar.baiganov@gmail.com>
2021-08-13 19:27:48 +0300

    Done sending data from Recipes fragment to Detail Activity

--- app/src/main/java/com/baiganov/foodapp/bindingadapters/RecipesRowBinding.kt
+++ app/src/main/java/com/baiganov/foodapp/bindingadapters/RecipesRowBinding.kt
@@ -1,14 +1,32 @@
 package com.baiganov.foodapp.bindingadapters
 
+import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.baiganov.foodapp.R
+import com.baiganov.foodapp.models.Result
+import com.baiganov.foodapp.ui.fragments.recipes.RecipesFragmentDirections
 
 object RecipesRowBinding {
+
+    @BindingAdapter("onRecipeClickListener")
+    @JvmStatic
+    fun onRecipeClickListener(layout: ConstraintLayout, result: Result) {
+        layout.setOnClickListener {
+            try {
+                val action = RecipesFragmentDirections.actionRecipesFragmentToDetailActivity(result)
+                layout.findNavController().navigate(action)
+            } catch (e: Exception) {
+                Log.d("onRecipeClickListener", e.toString())
+            }
+        }
+    }
 
     @BindingAdapter("loadImageFromUrl")
     @JvmStatic
