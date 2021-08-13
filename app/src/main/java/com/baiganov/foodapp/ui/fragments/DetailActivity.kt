b47fa44a6cd44c16c177fc225d5e6d0ecce6ac33
Данияр <daniyar.baiganov@gmail.com>
2021-08-13 19:05:10 +0300

    Done xml for detail activity

---
+++ app/src/main/java/com/baiganov/foodapp/ui/fragments/DetailActivity.kt
@@ -0,0 +1,12 @@
+package com.baiganov.foodapp.ui.fragments
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.baiganov.foodapp.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+    }
+}
