4e70f316a98617ab9892e7cfcdf68cc7e0a01aec
Данияр <daniyar.baiganov@gmail.com>
2021-08-17 20:26:37 +0300

    Done instructions fragment

--- app/src/main/java/com/baiganov/foodapp/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/com/baiganov/foodapp/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,16 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import com.baiganov.foodapp.R
+import com.baiganov.foodapp.databinding.FragmentIngredientsBinding
+import com.baiganov.foodapp.databinding.FragmentInstructionsBinding
+import com.baiganov.foodapp.databinding.FragmentOverviewBinding
+import com.baiganov.foodapp.models.Result
 
 
 class InstructionsFragment : Fragment() {
 
+    private var _binding: FragmentInstructionsBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+    ): View {
+        _binding = FragmentInstructionsBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+        val args = arguments
+        val model: Result? = args?.getParcelable("recipeBundle")
+
+        binding.instructionsWebView.webViewClient = object : WebViewClient() {}
+        val websiteUrl: String = model!!.sourceUrl
+        binding.instructionsWebView.loadUrl(websiteUrl)
+        return binding.root
     }
 }
